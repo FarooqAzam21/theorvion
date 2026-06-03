19d65d859ffe3473b6dc6c9a67404aa041c35232
Farooq Azam <azamfarooq891@gmail.com>
2026-06-03 15:02:04 +0500

    Removing google fonts

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
+import '@fontsource/inter';
+import '@fontsource/sora';
+import '@fontsource/poppins';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client';
 import { HelmetProvider } from "react-helmet-async";
-import '@fontsource/inter';
-import '@fontsource/sora';
-import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
